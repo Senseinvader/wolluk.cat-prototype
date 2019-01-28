e80c6e6d89e8debb6464661c8393034a3fd0a7e6
Senseinvader <d.ivantsov1983@gmail.com>
2019-01-29 00:41:35 +0100

    Add array with users to localStorage via users module of Store

--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -1,5 +1,24 @@
 const state = {
-
+  registeredUsers: [
+    {id: '-LWMS-pfJ937K4iwRl7Q', // this is id similar to Firebase (auto gen.)
+      displayName: 'Tomek Drazek',
+      email: 'tomek@wolluk.com',
+      password: 'pass',
+      roles: {
+        admin: true,
+        editor: true,
+        translator: true,
+        designer: true}},
+    {id: '-LWMS-pfJ937K4i4001l7Q', // this is id similar to Firebase (auto gen.)
+      displayName: 'Janusz Cebula',
+      email: 'janusz@example.com',
+      password: 'pass',
+      roles: {
+        admin: false,
+        editor: true,
+        translator: false,
+        designer: false}}
+  ]
 }
 const getters = {
 
