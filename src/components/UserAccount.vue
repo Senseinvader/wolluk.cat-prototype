f1777be33b1aaeccfbb1c7dfa6551a0b4a97a07b
Senseinvader <d.ivantsov1983@gmail.com>
2019-01-29 23:27:17 +0100

    Implement changing user password with verification

--- src/components/UserAccount.vue
+++ src/components/UserAccount.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-layout row wrap justify-center mt-5>
-      <v-flex xs7 md1 mr-4 justify-center>
+      <v-flex xs7 md1 mr-4>
         <v-btn large fab class='green'>
           <v-icon large color='white'>person</v-icon>
         </v-btn>
@@ -15,7 +15,20 @@
           <v-checkbox v-model="user.roles.translator" label='Translator' disabled ></v-checkbox>
           <v-checkbox v-model="user.roles.designer" label='Designer' disabled ></v-checkbox>
           <v-layout row wrap justify-center mb-3>
-            <v-btn flat class='red--text'>reset password</v-btn>
+            <v-btn flat class='red--text' @click='openPassForm'>reset password</v-btn>
+          </v-layout>
+          <v-layout row wrap justify-center>
+            <v-flex xs12 md12 v-if="passForm">
+              <v-form ref='form' v-model='valid'>
+                <v-text-field label="Old Password" :rules="oldPassRules" required></v-text-field>
+                <v-text-field label="New Password" v-model='match' required></v-text-field>
+                <v-text-field label="Repeat new Password" v-model='model' :rules='newPassRules' required></v-text-field>
+                <v-layout row wrap justify-center mb-4>
+                  <v-btn flat color='green' @click="closePassForm">cancel</v-btn>
+                  <v-btn dark color='green' @click='handleChangePass' :disabled='!valid'>submit changes</v-btn>  
+                </v-layout>            
+              </v-form>
+            </v-flex>
           </v-layout>
           <v-divider></v-divider>
           <v-layout row wrap justify-center pa-4>
@@ -40,25 +53,44 @@ export default {
         v => !!v || 'Name is required',
         v => (v && v.length < 20) || 'Name must be less than 20 characters'
       ],
-      user: null
+      oldPassRules: [
+        v => !!v || 'Field is reuquired',
+        v => (v === this.user.password) || 'Wrong current password'
+      ],
+      newPassRules: [
+        v => v === this.match || 'Passwords dont match'
+      ],
+      user: null,
+      passForm: false,
+      match: '',
+      model: ''
     }
+  },
+  watch: {
+    match: 'validateField',
+    model: 'validateField'
   },
   created () {
     this.user = this.$store.getters['auth/activeUser']
-  },
-  computed: {
-    signedInUser () {
-      return this.$store.getters['auth/activeUser']
-    },
-    signedInUserRoles () {
-      return this.$store.getters['auth/activeUserRoles']
-      // return this.signedInUser ? this.signedInUser.roles : null
-    }
+    console.log(this.user.password)
   },
   methods: {
     handleUpdate () {
       this.$store.dispatch('users/mutateUser', this.user)
       console.log(this.user.displayName)
+    },
+    openPassForm () {
+      this.passForm = true
+    },
+    closePassForm () {
+      this.passForm = false
+    },
+    handleChangePass () {
+      this.passForm = false
+      this.user.password = this.model
+    },
+    validateField () {
+      this.$refs.form.validate()
     }
   }
 }
